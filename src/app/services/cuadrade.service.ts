6e3d591ecee139e5a79280ce82186fc557cce152
Johan Díaz <jgiandh46@gmail.com>
2021-06-27 03:06:50 -0500

    :art: [ADD] Services para lógica de triángulos

--- src/app/services/cuadrade.service.ts
+++ src/app/services/cuadrade.service.ts
@@ -1,16 +1,14 @@
-
-
 export class Cuadrilatero {
     base: number;
     altura?: number;
 
-    constructor(b:number , h:number) {
+    constructor(b:number , h?:number) {
         this.base = b;
         this.altura = h;
     }
 
     calcPrAndAr(figure: Cuadrilatero) {
-        if(this.altura) {
+        if(this.altura || this.base == this.altura) {
             return this.base * this.altura;
         }
         else {
