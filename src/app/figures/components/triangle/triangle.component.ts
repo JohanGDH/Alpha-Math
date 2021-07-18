b33105b8792c909abf3a3ad96e69484441a662b2
Johan Díaz <jgiandh46@gmail.com>
2021-07-18 00:39:27 -0500

    :art: [ADD] Terminado TriangleComponent

--- src/app/figures/components/triangle/triangle.component.ts
+++ src/app/figures/components/triangle/triangle.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Triangle } from 'src/app/core/services/triangle.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-triangle',
   templateUrl: './triangle.component.html',
@@ -7,9 +8,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TriangleComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  triangle: Triangle;
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { 
+    this.buildForm()
+  }
 
   ngOnInit(): void {
   }
+
+  save($event:any) {
+    if(this.form.valid) {
+      this.triangle = new Triangle(this.baseField.value, this.lado1Field.value, this.lado2Field.value, this.alturaField.value)
+   
+      console.log(this.triangle.calcAr())
+    }
+  }
+
+  private buildForm() {
+    this.form = this.formBuilder.group({
+      base: [, [ Validators.required, Validators.min(0)]],
+      lado1: [, [Validators.required, Validators.min(0)]],
+      lado2: [, [Validators.min(0)]],
+      altura: [, [Validators.min(0)]],
+    })
+  }
+
+  get baseField() {
+    return this.form.get('base')
+  }
+
+  get lado1Field() {
+    return this.form.get('lado1')
+  }
+
+  get lado2Field() {
+    return this.form.get('lado2')
+  }
+
+  get alturaField() {
+    return this.form.get('altura')
+  }
+
 
 }
