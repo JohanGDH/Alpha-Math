f79de1728f41b1d811141eace1bb0fde32e3452a
Johan Díaz <jgiandh46@gmail.com>
2021-07-17 23:02:46 -0500

    :art: [ADD] TriangleComponent creado

--- src/app/figures/figures-routing.module.ts
+++ src/app/figures/figures-routing.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CircleComponent } from './components/circle/circle.component';
 import { CuadradeComponent } from './components/cuadrade/cuadrade.component';
+import { TriangleComponent } from './components/triangle/triangle.component';
 
 const routes: Routes = [
   {
-    path: 'cuadrado', 
+    path: 'cuadrade', 
     component: CuadradeComponent,
   },
   {
     path: 'circle', 
     component: CircleComponent,
+  },
+  {
+    path: 'triangle',
+    component: TriangleComponent
   }
 ];
 
--- src/app/figures/figures.module.ts
+++ src/app/figures/figures.module.ts
@@ -8,12 +8,14 @@ import { MaterialModule } from '../material/material.module';
 import { CuadradeComponent } from './components/cuadrade/cuadrade.component';
 import { FiguresRoutingModule } from './figures-routing.module';
 import { CircleComponent } from './components/circle/circle.component';
+import { TriangleComponent } from './components/triangle/triangle.component';
 
 
 @NgModule({
   declarations: [
     CuadradeComponent,
     CircleComponent,
+    TriangleComponent,
   ],
   imports: [
     CommonModule,
