ffb57d3d2c803187e038392b512ce9ce200ea8d0
Johan Díaz <jgiandh46@gmail.com>
2021-07-18 23:11:30 -0500

    :art: [ADD] Componentes del HomeModule creados

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    BannerComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule
   ]
