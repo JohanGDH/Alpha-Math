32cd15f9f8cba4e05ba3e3c82121d753b3417bbe
Johan Díaz <jgiandh46@gmail.com>
2021-07-24 22:31:47 -0500

    :art: [ADD] Terminado About y 404 Component

--- src/app/error-404/error-404.module.ts
+++ src/app/error-404/error-404.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { Error404RoutingModule } from './error-404-routing.module';
 import { Error404Component } from './components/error404/error404.component';
-
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +11,8 @@ import { Error404Component } from './components/error404/error404.component';
   ],
   imports: [
     CommonModule,
-    Error404RoutingModule
+    Error404RoutingModule,
+    MaterialModule
   ]
 })
 export class Error404Module { }
